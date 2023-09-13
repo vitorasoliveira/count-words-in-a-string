e72d495e2d12d7a866009d65cdf72ce09fad7b23
Vitor Alex <128174878+vitorasoliveira@users.noreply.github.com>
2023-09-13 15:37:56 -0300

    Add files via upload

---
+++ count-words-in-a-string.java
@@ -0,0 +1,9 @@
+// count the number of words in a string
+
+public class Main {
+    public static void main(String[]args) {
+        String words = "One Two Three Four";
+        int countWords = words.split("\\s").length;
+        System.out.println(countWords);
+    }
+}
